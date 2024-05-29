19cca14e9b5f5007e8b60f73d5020f993f37754c
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2024-05-29 23:31:43 +0300

    update main branch

---
+++ code/problem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+}
